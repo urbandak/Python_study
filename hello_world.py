d9f3d7c1a7ac688c95e75de49c82154afea4ab91
Urbandak <usrbanfox@163.com>
2018-05-29 20:17:56 +0800

    add more samples

--- hello_world.py
+++ hello_world.py
@@ -107,3 +107,35 @@ for car in cars:
     print(car.title() + "\t")
     print("I can't want to see you next trick " + car.title())
 print("this is end!!!")
+
+for value in range(1,5):
+    print(value)
+
+numbers = list(range(1,6))
+print(numbers)
+
+even_numbers = list(range(2,11,2))
+print(even_numbers)
+
+
+squares = []
+for value in range(0,11):
+    square = value **2
+    squares.append(square)
+print(squares)
+
+
+digits = [1,2,3,4,5,6,7,8,9,0]
+print(min(digits))
+
+print(max(digits))
+
+print(sum(digits))
+
+
+
+
+squares = [value**2 for value in range(1,11)]
+print(squares)
+
+
